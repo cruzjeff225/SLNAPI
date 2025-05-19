acf78e3bb65e7b7d9f676f7dc5b955db5ed6cd37
cruzjeff225 <cruzjeff225@gmail.com>
2025-05-19 10:03:29 -0600

    Agregando logica para crear y almacenar personas

---
+++ Apliccation/Features/Personas/Commands/CreatePersonaCommandHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apliccation.Features.Personas.Commands
+{
+    internal class CreatePersonaCommandHandler
+    {
+    }
+}
